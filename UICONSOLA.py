ba6b09eb370841d3a6d703054273288128e49b24
RamQ216 <hugomramirez06@gmail.com>
2026-03-20 13:24:07 -0300

    Update UICONSOLA.py

--- UICONSOLA.py
+++ UICONSOLA.py
@@ -1,5 +1,18 @@
-from MOTOR import JUEGO
+class Matriz:
+    def __init__(self):
+        # Mantengo tus nombres de variables
+        self.fila = int(input("ingrese un numero de filas: "))
+        self.columna = int(input("ingrese un numero de columnas: "))
+        self.__tablero = [] # Encapsulado con __ para cumplir el requisito
 
-if __name__ == "__main__":
-    partida = JUEGO()
-    partida.iniciar()
+    @property
+    def tablero(self):
+        return self.__tablero
+
+    def crear(self):
+        self.__tablero = [["." for _ in range(self.columna)] for _ in range(self.fila)]
+        return self.__tablero
+    
+    def mostrar(self):
+        for i in self.__tablero:
+            print(" ".join(i))
